298a65ce2ac2ab828d3eed197bc307c6232c2450
Aaron Walker <aaron45587@gmail.com>
2021-01-27 22:44:24 +0000

    Added basic wallet search functionality, removed hard coded wallet

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 from python import bitcoin_wallet as btc
+from forms import forms
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def homepage():
     wallet = "3QJmV3qfvL9SuYo34YihAf3sRCW3qSinyC"
-    balance = btc.get_curent_balance("3QJmV3qfvL9SuYo34YihAf3sRCW3qSinyC")
-    sent = btc.get_total_sent("3QJmV3qfvL9SuYo34YihAf3sRCW3qSinyC")
-    recieved = btc.get_total_received("3QJmV3qfvL9SuYo34YihAf3sRCW3qSinyC")
-    first_seen = btc.get_first_seen("3QJmV3qfvL9SuYo34YihAf3sRCW3qSinyC")
-    return render_template("index.html", title="Hashboard", wallet=wallet, balance=balance, sent=sent, received=recieved, first_seen=first_seen)
+
+    search = forms.WalletSearchForm(request.form)
+    if request.method == 'POST':
+        return search_results(search)
+
+    return render_template("index.html", form=search, title="Hashboard")
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def search_results(search):
+    search_string = search.data['search']
+    wallet = btc.Wallet(search_string)
+
+    # if search.data['search'] == '':
+    #     qry = db_session.query(Album)
+    #     results = qry.all()
+    # if not results:
+    #     flash('No results found!')
+    #     return redirect('/')
+    # else:
+    #     # display results
+
+    search = forms.WalletSearchForm(request.form)
+
+    return render_template('results.html', results=search_string, wallet=wallet, form=search, title="Hashboard")
 
 
 if __name__ == "__main__":
